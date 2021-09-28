3b8b2f7e1a955a629765b6a6cb6fc34eb1a962d0
Arshadfaz20 <52900648+Arshadfaz20@users.noreply.github.com>
2021-09-28 20:15:28 +1000

    Updated Testbench, Arshad/Ishan/Pravindu

--- testbench.sv
+++ testbench.sv
@@ -1,40 +1,73 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns / 1ps
 module Cycle_Cooling_System_TB;
 
- reg reset;
-  reg[2:0] calorie,temperature;
- reg  pressure,air_pressure;
- wire fan;
+ reg rst, clk;
+ reg[2:0] calorie,temperature,pressure,air_pressure;
+  wire [2:0] fan_mode;
 
  Cycle_Cooling_System dut(
 
- .calorie(calorie),
- .temperature(temperature),
- .pressure(pressure),
- .air_pressure(air_pressure),
- .fan(fan)
+   .clk(clk),
+   .rst(rst),
+   .calorie(calorie),
+   .temperature(temperature),
+   .pressure(pressure),
+   .air_pressure(air_pressure),
+   .fan_mode(fan_mode)
  );
-
+initial 
+  begin
+    clk = 1'b0;
+    forever #(1000000000/2) clk =~ clk;
+  end
 
  initial
  begin
  $dumpfile("dump.vcd");
  $dumpvars;
 
+   rst = 0;
  calorie = 3'b010;
  temperature =3'b010;
  pressure = 1'b1;
  air_pressure = 1'b1; 
- #1000;
-   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan = %d", calorie, temperature, pressure, air_pressure, fan);
- calorie = 3'b001;
+ #1000000000;
+   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan_mode = %d", calorie, temperature, pressure, air_pressure, fan_mode);
+   rst = 1;
+ calorie = 3'b010;
  temperature =3'b010;
- pressure = 0;
- air_pressure = 0; 
- #1000;
-   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan = %d", calorie, temperature, pressure, air_pressure, fan);
+ pressure = 3'b010;
+ air_pressure = 3'b010; 
+ #1000000000;
+   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan_mode = %d", calorie, temperature, pressure, air_pressure, fan_mode);
+   rst = 0;
+   calorie = 3'b010;
+ temperature =3'b010;
+ pressure = 3'b010;
+ air_pressure = 3'b010;
+   #(1000000000*200);
    
+   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan_mode = %d", calorie, temperature, pressure, air_pressure, fan_mode);
+   rst = 1;
+ calorie = 3'b010;
+ temperature =3'b010;
+ pressure = 3'b000;
+ air_pressure = 3'b010; 
+ #1000000000;
+   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan_mode = %d", calorie, temperature, pressure, air_pressure, fan_mode);
+   rst = 0;
+   calorie = 3'b010;
+ temperature =3'b010;
+ pressure = 3'b000;
+ air_pressure = 3'b010;
+   #(1000000000*200);
+   
+   $display (" calorie = %d, temerature = %d, pressure = %d, air_pressure = %d, fan_mode = %d", calorie, temperature, pressure, air_pressure, fan_mode);
  $finish;
+   
+   
    
  end
 endmodule
